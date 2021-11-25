9218fd5ece295a511a35d556008da0f9b76c9601
rajiv pratap singh <rajivpratapsingh1999@gmail.com>
2021-11-26 02:02:42 +0530

    push

---
+++ src/links/utils.py
@@ -0,0 +1,21 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+def get_link_data(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(response.text, "lxml")
+
+    name = soup.select_one(selector="#productTitle").getText()
+    name = name.strip()
+
+    price = soup.select_one(selector="#priceblock_ourprice").getText()
+    price = float(price[1:])
+
+    return name, price
